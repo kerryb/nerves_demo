b7d640a5bcb9db61cf98d7c9e98956fbbd004d79
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-03 21:32:40 +0100

    Parameterise HTML and set initial state

--- demo_ui/lib/demo_ui_web/live/main_live.ex
+++ demo_ui/lib/demo_ui_web/live/main_live.ex
@@ -7,13 +7,20 @@ defmodule DemoUiWeb.MainLive do
     ~H"""
     <h1>Nerves demo</h1>
     <form>
-      <button class="button" name="toggle-led">Turn LED on</button>
-      <p>Switch is <span class="off">OFF</span></p>
+      <button class="button" name="toggle-led">Turn LED <%= if @led_on?, do: "OFF", else: "ON" %></button>
+      <p>
+        Switch is
+        <%= if @switch_on? do %>
+          <span class="on">ON</span>
+        <% else %>
+          <span class="off">OFF</span>
+        <% end %>
+      </p>
     </form>
     """
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, led_on?: false, switch_on?: false)}
   end
 end
