bc4ccbaa3eb0a1374595336cde7646b0dc5bc8d1
Kerry Buckley <kerryjbuckley@gmail.com>
2021-10-05 12:40:56 +0100

    Monitor switch events and broadcast updates

--- demo_fw/lib/demo_fw.ex
+++ demo_fw/lib/demo_fw.ex
@@ -3,19 +3,31 @@ defmodule DemoFw do
 
   alias Circuits.GPIO
 
+  @led_pin 4
+  @switch_pin 5
+
   def start_link(args) do
     GenServer.start_link(__MODULE__, args)
   end
 
   def init(_) do
     :ok = Phoenix.PubSub.subscribe(DemoUi.PubSub, "commands")
-    {:ok, led} = GPIO.open(4, :output)
-    {:ok, %{led: led, led_on?: false}}
+    {:ok, led} = GPIO.open(@led_pin, :output)
+    {:ok, switch} = GPIO.open(@switch_pin, :input)
+    :ok = GPIO.set_interrupts(switch, :both)
+    # Note: we need to store switch gpio ref in state to stop it being
+    # garbage-collected, which would mean we only received the first message
+    {:ok, %{led: led, switch: switch, led_on?: false}}
   end
 
   def handle_info(:toggle_led, state) do
     led_on? = not state.led_on?
     GPIO.write(state.led, if(led_on?, do: 1, else: 0))
     {:noreply, %{state | led_on?: led_on?}}
+  end
+
+  def handle_info({:circuits_gpio, @switch_pin, _timestamp, switch_position}, state) do
+    Phoenix.PubSub.broadcast!(DemoUi.PubSub, "updates", {:switch, switch_position == 1})
+    {:noreply, state}
   end
 end
